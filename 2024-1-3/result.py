d730ef026d5109cbdcc500e0ca0bd6716a931e60
corn -ubuntu <happpy.corn.man@gmail.com>
2024-05-23 09:24:47 +0800

    cancel for

--- 2024-1-3/result.py
+++ 2024-1-3/result.py
@@ -1,13 +1,16 @@
-p, q, r, m = map(int, input().split())
+p, q, r, m = [int(i) for i in input().split()]
 
-input_result = list(map(int, input().split()))
-result = [[0, i] for i in input_result]
+result = [[-1, -1]] + [[0, int(i)] for i in input().split()] + [[-1, -1]] * q
 
-result.extend([[None, None]] * q)
+logic_gat = [0] + [int(i) for i in input().split()]
 
-logic_gat = list(map(int, input().split()))
+lines = [[] for _ in range(q+r+1)]
 
-lines = [tuple(map(int, input().split())) for _ in range(m)]
+for _ in range(m):
+
+    value, key = [int(i) for i in input().split()]
+
+    lines[key - p].append(value)
 
 logics = {
     1 : lambda x, y : x and y,
@@ -20,44 +23,31 @@ def decide(gat_number):
 
     global result
 
-    load = []
+    logic_function = logics[logic_gat[gat_number-p]]
+    input_index = lines[gat_number - p]
 
-    for i in lines:
+    for i in input_index:
 
-        if i[1] == gat_number:
+        if result[i][0] < 0:
+            decide(i)
+        
+    inputs = [result[i] for i in input_index]
 
+    depth = max([i[0] for i in inputs]) + 1
 
-            if result[i[0]-1][0] is None:
+    arg = [i[1] for i in inputs]
+    result[gat_number] = [depth, int(logic_function(*arg))]
 
-                decide(i[0])
-
-            load.append(result[i[0]-1])
-
-            if len(load) > 1:
-
-                break
-
-    arg = [i[1] for i in load]
-
-    depth = max([i[0] for i in load]) + 1
-
-    logic = logics[logic_gat[gat_number-p-1]]
-
-    result[gat_number-1] = [depth, int(logic(*arg))]
-
-output_number = range(p + q + 1, p + q + r + 1)
-input_number = [(i[1],i[0]) for i in lines if i[1] in output_number]
-input_number.sort()
-output_value = [None] * len(input_number)
+output_index = range(q + 1, q + r + 1)
+input_number = [lines[i][0] for i in output_index]
+output_value = [-1] * len(input_number)
 
 for key, value in enumerate(input_number):
 
-    if result[value[1]-1][0] is None:
-
-        decide(value[1])
+    if result[value][0] < 0:
+        decide(value)
     
-    output_value[key] = result[value[1]-1][1]
+    output_value[key] = result[value][1]
 
 print(max(result, key=lambda x: x[0])[0])
-
 print(" ".join(map(str, output_value)))
