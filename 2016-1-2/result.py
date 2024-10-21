dad97ffa7e21c7236030b686d9137c369fe391b1
happpycorn <happpy.corn.man@gmail.com>
2024-10-21 16:42:58 +0800

    fix bug

--- 2016-1-2/result.py
+++ 2016-1-2/result.py
@@ -1,26 +1,18 @@
 r, c, m = map(int, input().split())
-
 a = [[int(i) for i in input().split()] for _ in range(r)]
-
 b = [int(i) for i in input().split()]
 
-def flip(a : list):
-    
+b.reverse()
+
+def flip(a : list):    
     return [a[-i-1] for i in range(len(a))]
 
 def turn(a : list):
-
     r, c = len(a), len(a[0])
-
-    return [[a[-i-1][j] for i in range(r)] for j in range(c)]
+    return [[a[i][-j-1] for i in range(r)] for j in range(c)]
 
 for i in b:
-
-    if i:
-        a = flip(a)
-        
-    else:
-        a = turn(a)
+    a = flip(a) if i else turn(a)
 
 r, c = len(a), len(a[0])
 
