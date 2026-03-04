c7817d0122b76290dbf5426d560c081a1fb65b2e
happpycorn <135821359+happpycorn@users.noreply.github.com>
2026-03-04 03:41:36 +0000

    update

--- CPE/01203.py
+++ CPE/01203.py
@@ -2,4 +2,20 @@ def main():
     from sys import stdin
     e = stdin.readline
 
+    import heapq
+    pq = []
+
+    while True:
+        while True:
+            s = e()
+            if not s: return
+            if s.strip() == '#': break
+            r, arg, p = s.split()
+            heapq.heappush(pq, (int(p), int(arg), int(p)))
+        
+        for _ in range(int(e().strip())):
+            t, arg, p = heapq.heappop(pq)
+            print(arg)
+            heapq.heappush(pq, (t+p, arg, p))
+
 main()
